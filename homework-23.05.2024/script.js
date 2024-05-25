79c1072c78cc7c4e829c9e80d2d0f9a78814f03d
dev-hemul <dev.hemulll@gmail.com>
2024-05-25 13:35:44 +0300

    add timer

--- homework-23.05.2024/script.js
+++ homework-23.05.2024/script.js
@@ -5,21 +5,33 @@ let err = document.querySelector(".err");
 let err_2 = document.querySelector(".err-2");
 let success = document.querySelector(".success");
 
+success.classList.add()
+
+
 form_button.addEventListener("click", (evt) => {
 	evt.preventDefault();
-		if (input_accept.value !== 'так') {
-			err_2.style.display = "block";
-			form_button.style.marginTop = "70px";
-			console.log(err);
-		}else if (input_accept.value === 'так' && input_name.value === '') {
-			err.style.display = "block";
-			form_button.style.marginTop = "70px";
-		}else {
-			err.style.display = "none";
-			err_2.style.display = "none";
-			success.style.display = "block";
-			form_button.style.marginTop = "50px";
+	if (input_accept.value !== 'так' && input_accept.value !== 'Так') {
+		err_2.style.display = "block";
+		form_button.style.marginTop = "70px";
+		console.log(err);
+	} else if (input_accept.value === 'так' && input_name.value === '') {
+		err.style.display = "block";
+		form_button.style.marginTop = "70px";
+	} else if (input_accept.value === 'Так' && input_name.value === '') {
+		err.style.display = "block";
+		form_button.style.marginTop = "70px";
+	} else {
+		err.style.display = "none";
+		err_2.style.display = "none";
+		success.style.display = "block";
+		form_button.style.marginTop = "50px";
+		function close() {
+			success.style.display = "none";
+			form_button.style.marginTop = "15px";
 		}
+
+		setTimeout(close, 5000);
+	}
 });
 
 
