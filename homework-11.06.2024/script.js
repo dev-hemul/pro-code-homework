ec4f45e9a4fc822b3cf42ea749e2044adbacd63f
dev-hemul <dev.hemulll@gmail.com>
2024-06-13 19:31:53 +0300

    finish 1lvl

--- homework-11.06.2024/script.js
+++ homework-11.06.2024/script.js
@@ -1,15 +1,15 @@
+let battery = document.querySelector('.battery');
+let battery_mode = document.querySelector('.battery-mode');
+
 document.querySelectorAll(".switch-check").forEach(switchCheck => {
     switchCheck.addEventListener("change", function() {
-				let battery = document.querySelector(".battery")
-        let battery_mode = document.querySelector(".battery-mode");
-        let label = this.nextElementSibling;
-        let elementStyle = getComputedStyle(label, "::after");
-
-        if (elementStyle.getPropertyValue("background-color") === "rgb(144, 220, 255)") {
-					battery_mode.classList.toggle("battery-mode-active");
-        } else if(battery.classList.contains("battery-mode-active")) {
-            battery_mode.classList.remove("battery-mode-active");
-            battery_mode.classList.add("battery-mode");
-        }
+        if (this.checked) {
+            let spanElement = this.nextElementSibling.querySelector('span');
+            if (spanElement) {
+							battery.classList.add('battery-mode-active');
+            }
+        }else {
+					battery.classList.remove('battery-mode-active');
+				}
     });
 });
