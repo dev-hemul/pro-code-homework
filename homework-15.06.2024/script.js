31a77f4ccf9177ea440f32a96db78069bb1f71ea
dev-hemul <dev.hemulll@gmail.com>
2024-06-18 17:44:36 +0300

    done homework

---
+++ homework-15.06.2024/script.js
@@ -0,0 +1,45 @@
+let input_pib = document.querySelector(".user-pib");
+let sort = document.querySelector(".form__sort");
+let form = document.querySelector(".form");
+let output = document.querySelector(".output");
+
+let arr = [];
+
+form.addEventListener("submit", event => {
+	event.preventDefault();
+	let arr_push = () => {
+		arr.push(input_pib.value)
+		return arr;
+	}
+	let updatedArr = arr_push();
+	displayOutput(updatedArr);
+	input_pib.value = '';
+})
+
+let displayOutput = (arr) => {
+            output.innerHTML = ''; // очищаем текущий вывод
+
+            arr.forEach(item => {
+                let li = document.createElement("li");
+                li.textContent = item;
+                output.appendChild(li);
+            });
+        };
+
+sort.addEventListener("click", event => {
+	event.preventDefault();
+	 arr.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+	displayOutput(arr);
+})
+
+
+
+
+
+
+
+
+
+
+
+
