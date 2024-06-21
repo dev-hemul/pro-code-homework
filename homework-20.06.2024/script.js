1841d915a37bf9468c1545d5a23603ad6c253b9c
dev-hemul <dev.hemulll@gmail.com>
2024-06-21 12:33:48 +0300

    bug fix-2

--- homework-20.06.2024/script.js
+++ homework-20.06.2024/script.js
@@ -1,4 +1,18 @@
- const dateInput = document.getElementById('date-input');
+function updatePlaceholderVisibility() {
+      const dateInput = document.getElementById('date-input');
+      const datePlaceholder = document.querySelector('.date-placeholder');
+      if (window.innerWidth <= 576 && !dateInput.value) {
+        datePlaceholder.style.display = 'block';
+      } else {
+        datePlaceholder.style.display = 'none';
+      }
+    }
+
+    // Обновить видимость при загрузке страницы и изменении размера окна
+    window.addEventListener('load', updatePlaceholderVisibility);
+    window.addEventListener('resize', updatePlaceholderVisibility);
+
+    const dateInput = document.getElementById('date-input');
     const datePlaceholder = document.querySelector('.date-placeholder');
 
     dateInput.addEventListener('focus', () => {
