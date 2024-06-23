27b80824d31557deef78cc06e7b18a0eee52bcf8
dev-hemul <dev.hemulll@gmail.com>
2024-06-23 21:23:52 +0300

    shortened the code

--- homework-20.06.2024/script.js
+++ homework-20.06.2024/script.js
@@ -8,6 +8,27 @@ let sortAZ = document.querySelector(".sort-AZ");
 
 let students = [];
 
+let createTable = () => {
+	const studTable = document.getElementById('stud-table');
+	studTable.innerHTML = '';
+
+	for (const student of students) {
+		const tr = document.createElement('tr');
+		const tdPib = document.createElement('td');
+		const tdAge = document.createElement('td');
+		const tdGender = document.createElement('td');
+		const tdBirthday = document.createElement('td');
+
+		tdPib.textContent = student.pib;
+		tdAge.textContent = student.age;
+		tdGender.textContent = student.gender;
+		tdBirthday.textContent = student.birthday;
+
+		tr.append(tdPib, tdAge, tdGender, tdBirthday);
+		studTable.append(tr);
+	}
+}
+
 form.addEventListener("submit", function (evt) {
 	evt.preventDefault();
 
@@ -23,72 +44,22 @@ form.addEventListener("submit", function (evt) {
 	age.value = '';
 	gender.value = '';
 	birthday.value = '';
-
-	const studTable = document.getElementById('stud-table');
-	studTable.innerHTML = '';
-
-	for (const student of students) {
-		const tr = document.createElement('tr');
-		const tdPib = document.createElement('td');
-		const tdAge = document.createElement('td');
-		const tdGender = document.createElement('td');
-		const tdBirthday = document.createElement('td');
-
-		tdPib.textContent = student.pib;
-		tdAge.textContent = student.age;
-		tdGender.textContent = student.gender;
-		tdBirthday.textContent = student.birthday;
-
-		tr.append(tdPib, tdAge, tdGender, tdBirthday);
-		studTable.append(tr);
-	}
+	createTable();
 
 });
 
 arrow.addEventListener("click", function (evt) {
 	evt.preventDefault();
   students.reverse();
-	const studTable = document.getElementById('stud-table');
-	studTable.innerHTML = '';
+	createTable();
 
-	for (const student of students) {
-		const tr = document.createElement('tr');
-		const tdPib = document.createElement('td');
-		const tdAge = document.createElement('td');
-		const tdGender = document.createElement('td');
-		const tdBirthday = document.createElement('td');
-
-		tdPib.textContent = student.pib;
-		tdAge.textContent = student.age;
-		tdGender.textContent = student.gender;
-		tdBirthday.textContent = student.birthday;
-
-		tr.append(tdPib, tdAge, tdGender, tdBirthday);
-		studTable.append(tr);
-	}
 });
 
 sortAZ.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	const studTable = document.getElementById('stud-table');
-	studTable.innerHTML = '';
 	students.sort((a, b) => a.pib.localeCompare(b.pib));
+	createTable();
 
-	for (const student of students) {
-		const tr = document.createElement('tr');
-		const tdPib = document.createElement('td');
-		const tdAge = document.createElement('td');
-		const tdGender = document.createElement('td');
-		const tdBirthday = document.createElement('td');
-
-		tdPib.textContent = student.pib;
-		tdAge.textContent = student.age;
-		tdGender.textContent = student.gender;
-		tdBirthday.textContent = student.birthday;
-
-		tr.append(tdPib, tdAge, tdGender, tdBirthday);
-		studTable.append(tr);
-	}
 });
 
 
