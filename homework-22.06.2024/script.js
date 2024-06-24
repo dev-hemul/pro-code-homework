e111302c30c429edc7a92b0b7648990a0c16dbea
dev-hemul <dev.hemulll@gmail.com>
2024-06-24 13:22:15 +0300

    done homework

---
+++ homework-22.06.2024/script.js
@@ -0,0 +1,42 @@
+let red = document.querySelector(".circle-red");
+let orange = document.querySelector(".circle-orange");
+let green = document.querySelector(".circle-green");
+let btn = document.querySelector(".circle");
+
+let colors = ["red", "orange", "green", "orange"];
+let currentIndex = 0;
+
+red.classList.add("active-red");
+
+let lastClickTime = 0;
+
+let changeColor = () => {
+    red.classList.remove("active-red");
+    orange.classList.remove("active-orange");
+    green.classList.remove("active-green");
+
+    if (colors[currentIndex] === "red") {
+        red.classList.add("active-red");
+    } else if (colors[currentIndex] === "orange") {
+        orange.classList.add("active-orange");
+    } else if (colors[currentIndex] === "green") {
+        green.classList.add("active-green");
+    }
+
+    currentIndex = (currentIndex + 1) % colors.length;
+}
+
+btn.addEventListener("click", () => {
+    let currentTime = new Date().getTime();
+    if (currentTime - lastClickTime >= 5000) {
+        green.classList.add("active-green");
+        red.classList.remove("active-red");
+        orange.classList.remove("active-orange");
+        currentIndex = 2;
+        lastClickTime = currentTime;
+    } else {
+        console.log("Зачекайте 5 секунд");
+    }
+});
+
+setInterval(changeColor, 3000);
