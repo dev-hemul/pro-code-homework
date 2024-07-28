18ee50d17cc3462ce9666021b08e120973e802dc
dev-hemul <dev.hemulll@gmail.com>
2024-07-28 21:29:04 +0300

    change routs

--- homework-23.07.2024/main.js
+++ homework-23.07.2024/main.js
@@ -1,10 +1,7 @@
 import express from 'express';
-import index from './routes/index.js';
-import about from './routes/about.js';
-import contact from './routes/contact.js';
-
-/*import blogRouter from './routes/blog.js';
-import prodRouter from './routes/prod.js';*/
+import MainRouter from './routes/index.js';
+import AboutRouter from './routes/about.js';
+import ContactRouter from './routes/contact.js';
 
 const app = express()
 const PORT = process.env.PORT || 8000;
@@ -16,9 +13,9 @@ app.use(express.static('static'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.use('/', index);
-app.use('/', about);
-app.use('/', contact);
+app.use('/', MainRouter);
+app.use('/about', AboutRouter);
+app.use('/contact', ContactRouter);
 
 
 
--- homework-23.07.2024/routes/about.js
+++ homework-23.07.2024/routes/about.js
@@ -2,7 +2,7 @@ import {Router} from "express";
 
 const router = new Router();
 
-router.get('/about', (req, res) => {
+router.get('/', (req, res) => {
     res.render('about');
 });
 
