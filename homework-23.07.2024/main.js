026805800511b5c5b78b1cc7179f31afb453fa2a
dev-hemul <dev.hemulll@gmail.com>
2024-07-29 16:36:58 +0300

    add social and err

--- homework-23.07.2024/main.js
+++ homework-23.07.2024/main.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import createHttpError from "http-errors";
 import MainRouter from './routes/index.js';
 import AboutRouter from './routes/about.js';
 import ContactRouter from './routes/contact.js';
+import SocialRouter from './routes/social.js';
 
 const app = express()
 const PORT = process.env.PORT || 8000;
@@ -9,12 +11,29 @@ app.listen(PORT, () => {
 	console.log(`Server started on ${PORT}`);
 })
 
-app.use(express.static('static'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.use(express.static('static'));
 
 app.use('/', MainRouter);
 app.use('/about', AboutRouter);
 app.use('/contact', ContactRouter);
+
+/*app.use('/social', SocialRouter);*/
+
+app.use((req, res, next) => {
+	next(createHttpError(404));
+})
+
+// error hendler - midleware для обробки помилок. Тобто спочатку вище формуємо помилку, а потім всі помилки передаються сюди
+app.use((err, req, res, next) => {
+    const { status = 404, message } = err; // Беремо статус помилки
+    console.error(status);
+    console.error(message);
+
+    res.status(status); // Встановлюємо статус відповіді
+    res.render('error', { errorStatus: status, message }); // Передаємо статус і повідомлення в шаблон
+});
 
 
