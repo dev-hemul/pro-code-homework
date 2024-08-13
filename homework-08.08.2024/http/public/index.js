eb8846b398a4d84093e70f05be9474ca55e6c855
dev-hemul <dev.hemulll@gmail.com>
2024-08-13 22:41:28 +0300

    update port

--- homework-08.08.2024/http/public/index.js
+++ homework-08.08.2024/http/public/index.js
@@ -28,8 +28,8 @@ formArea.addEventListener("keydown", (e) => {
     }
 });
 
-const url = 'wss://jellyfish-app-xsopg.ondigitalocean.app';
-const socket = new WebSocket(url);
+const protocol = window.location.protocol.includes('https') ? 'wss': 'ws';
+const socket = new WebSocket(`${protocol}://${location.host}`);
 
 socket.onmessage = (e) => {
     console.log('Received data:', e.data);
@@ -43,4 +43,3 @@ socket.onmessage = (e) => {
 socket.onopen = (e) => {
     console.log("WebSocket connection established");
 }
-
