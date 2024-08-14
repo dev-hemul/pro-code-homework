0d613e4f75ab9e57c5e726fb6fce409217c7cc18
dev-hemul <dev.hemulll@gmail.com>
2024-08-14 09:11:05 +0300

    update port

--- homework-08.08.2024/http/public/index.js
+++ homework-08.08.2024/http/public/index.js
@@ -31,6 +31,7 @@ formArea.addEventListener("keydown", (e) => {
 const protocol = window.location.protocol.includes('https') ? 'wss' : 'ws'
 const socket = new WebSocket(`${protocol}://${location.host}`);
 
+
 /*const url = 'ws://localhost:7000';
 const socket = new WebSocket(url);*/
 
