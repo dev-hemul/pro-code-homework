35bcc2ea4dd9e69c5078592f1c38c2af831f6ebb
dev-hemul <dev.hemulll@gmail.com>
2024-08-14 09:03:16 +0300

    update port

--- homework-08.08.2024/bin/runners/http.js
+++ homework-08.08.2024/bin/runners/http.js
@@ -3,7 +3,7 @@ import server from "../../http/server.js";
 
 export default function startServer() {
   const httpServer = http.createServer(server);
-  const PORT = process.env.PORT || 8000;
+  const PORT = 8000;
 
   httpServer.listen(PORT, () => {
     console.log(`HTTP Server is running on port ${PORT}`);
