407a9db396fbdd8a5820b4c969d0ec4a9853badc
dev-hemul <dev.hemulll@gmail.com>
2024-08-14 08:07:19 +0300

    update port

--- homework-08.08.2024/http/public/index.js
+++ homework-08.08.2024/http/public/index.js
@@ -3,44 +3,47 @@ let messagesDiv = document.getElementById("messages");
 let formArea = document.querySelector(".form__area");
 
 let sendMessage = () => {
-    let formData = new FormData(form);
-    let formDataValue = formData.get("text"); // Получаем значение конкретного поля, например "text"
+	let formData = new FormData(form);
+	let formDataValue = formData.get("text"); // Получаем значение конкретного поля, например "text"
 
-    console.log("Отправляем значение:", formDataValue);
+	console.log("Отправляем значение:", formDataValue);
 
-    if (socket.readyState === WebSocket.OPEN) {
-        socket.send(formDataValue); // Отправляем только значение поля
-        formArea.value = "";
-    } else {
-        console.log("WebSocket not connected");
-    }
+	if (socket.readyState === WebSocket.OPEN) {
+		socket.send(formDataValue); // Отправляем только значение поля
+		formArea.value = "";
+	} else {
+		console.log("WebSocket not connected");
+	}
 }
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    sendMessage();
+	e.preventDefault();
+	sendMessage();
 });
 
 formArea.addEventListener("keydown", (e) => {
-    if (e.key === "Enter" && !e.shiftKey) {
-        e.preventDefault();
-        sendMessage();
-    }
+	if (e.key === "Enter" && !e.shiftKey) {
+		e.preventDefault();
+		sendMessage();
+	}
 });
 
-const url = 'ws://localhost:7000';
-const socket = new WebSocket(url);
+const protocol = window.location.protocol.includes('https') ? 'wss' : 'ws'
+const socket = new WebSocket(`${protocol}://${location.host}`);
+
+/*const url = 'ws://localhost:7000';
+const socket = new WebSocket(url);*/
 
 socket.onmessage = (e) => {
-    console.log('Received data:', e.data);
+	console.log('Received data:', e.data);
 
-    let messageElement = document.createElement('p');
-    messageElement.textContent = e.data;
+	let messageElement = document.createElement('p');
+	messageElement.textContent = e.data;
 
-    messagesDiv.appendChild(messageElement);
+	messagesDiv.appendChild(messageElement);
 }
 
 socket.onopen = (e) => {
-    console.log("WebSocket connection established");
+	console.log("WebSocket connection established");
 }
 
