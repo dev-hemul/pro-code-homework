d2d3cc155a84fc0237bec9d182c6589e5da359f3
dev-hemul <dev.hemulll@gmail.com>
2024-08-29 16:13:15 +0300

    add style to article

--- homework-24.08.2024/src/App.js
+++ homework-24.08.2024/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import Button from './Components/Button';
+import Text_field from './Components/Text_field';
+import Input from './Components/Input';
+
 
 function App() {
   return (
     <div>
+    <Button>
+    
+    </Button>
+    <Text_field>
+    
+    </Text_field>
+    
     
     </div>
   );
